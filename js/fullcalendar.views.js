1ebf12b93968eeffb960929251d31ed8e8714d4d
Tim Plunkett <git@plnktt.com>
2011-07-02 15:53:21 -0400

    Issue #1206518 by tim.plunkett: Call a specific calendar view through the URL.

--- js/fullcalendar.views.js
+++ js/fullcalendar.views.js
@@ -75,8 +75,8 @@ Drupal.behaviors.fullCalendar = function(context) {
         return false;
       },
       year: (settings.year) ? settings.year : undefined,
-      month: (settings.month) ? settings.month : undefined,
-      day: (settings.day) ? settings.day : undefined,
+      month: (settings.month) ? settings.month - 1 : undefined,
+      date: (settings.day) ? settings.day : undefined,
       timeFormat: {
         agenda: (settings.clock) ? 'HH:mm{ - HH:mm}' : settings.agenda,
         '': (settings.clock) ? 'HH:mm' : settings.agenda
